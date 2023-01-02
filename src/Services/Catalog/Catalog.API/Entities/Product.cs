8093b583d6a37cb3f6e300c1d6f2e4213d283877
Fábio Camillo <fabiocamillo@outlook.com>
2023-01-02 11:10:21 -0300

    Create entity product and add mongo Driver

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
